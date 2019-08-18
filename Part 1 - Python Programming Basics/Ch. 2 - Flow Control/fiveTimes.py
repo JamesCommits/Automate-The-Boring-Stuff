213a47ab276d100b97ffc13227a7f636beb0f9a0
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-18 15:55:33 -0400

    Helping see a for loop in action

---
+++ Part 1 - Python Programming Basics/Ch. 2 - Flow Control/fiveTimes.py
@@ -0,0 +1,10 @@
+# This program will help us see a for loop in action
+print('My Name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+# Here is another loop example
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
