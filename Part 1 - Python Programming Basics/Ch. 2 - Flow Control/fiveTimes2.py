e52e7bd5615a97997969d5df09efc469dfafb3c4
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-18 16:05:34 -0400

    A while loop equivalent of a for loop

---
+++ Part 1 - Python Programming Basics/Ch. 2 - Flow Control/fiveTimes2.py
@@ -0,0 +1,6 @@
+# In this instance of fiveTimes we'll use a while loop equivalent of a for loop
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
