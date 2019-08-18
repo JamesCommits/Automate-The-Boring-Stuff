bec953fe40b1fe2a1fb21250190e751b0b6a9af3
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-18 16:13:33 -0400

    Using the random module and importing it

---
+++ Part 1 - Python Programming Basics/Ch. 2 - Flow Control/printRandom.py
@@ -0,0 +1,6 @@
+# Importing the random function which will give us access
+# to the random.randint() function to use
+import random
+for i in range(5):
+    # Calling the random module and using the randomint function
+    print(random.randint(1, 10))
