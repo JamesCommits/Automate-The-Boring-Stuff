e9f76f6ef2e1b7dac1fbb92520242049a7169c34
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-19 13:48:28 -0400

    Ch. 4 Practice project

--- Part 1 - Python Programming Basics/Ch. 4 - Lists/characterPictureGrid.py
+++ Part 1 - Python Programming Basics/Ch. 4 - Lists/characterPictureGrid.py
@@ -8,6 +8,10 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+# You can think of grid[x][y] as being the character at the x- and y- coordinates of a 'picture' drawn with text characters.
+# The (0,0) origin will be in the upper-left corner, the x-coordinates increase going right and the y-coordinates
+# increase going down.  Copy the previous grid value, and write code that uses it to print the image
+
 for j in range(len(grid[0])):
     for i in range(len(grid)):
         print(grid[i][j],end='')
