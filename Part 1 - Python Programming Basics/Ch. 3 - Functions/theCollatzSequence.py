143d7c930581175d92748ec7096ea1690f953f34
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-18 21:12:58 -0400

    Progress on collatzSequence project

--- Part 1 - Python Programming Basics/Ch. 3 - Functions/theCollatzSequence.py
+++ Part 1 - Python Programming Basics/Ch. 3 - Functions/theCollatzSequence.py
@@ -1,3 +1,14 @@
 def collatz(number):
     if number % 2 == 0:
-        print()
+        even = number // 2
+        print(even)
+        return even
+    else:
+        odd = 3 * number +1
+        print(odd)
+        return odd
+
+userNumber = input("What's your Collatz number?")
+userNumberCollatz = collatz(userNumber)
+if userNumberCollatz > 1:
+    
