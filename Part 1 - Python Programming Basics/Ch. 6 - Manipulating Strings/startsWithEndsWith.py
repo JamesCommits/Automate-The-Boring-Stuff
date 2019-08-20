e2158f02a88a8042994f4ab410fdb1837cd8b6db
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-20 15:35:26 -0400

    startswith() and endswith() method examples

---
+++ Part 1 - Python Programming Basics/Ch. 6 - Manipulating Strings/startsWithEndsWith.py
@@ -0,0 +1,6 @@
+# The startswith() and endswith() methods return True if the string value they are called on
+# begins or ends (respectively) with the string passed to the method; otherwise, they return
+# False.
+
+'Hello world!'.startswith('Hello')
+'Hello world'.endswith('world')
