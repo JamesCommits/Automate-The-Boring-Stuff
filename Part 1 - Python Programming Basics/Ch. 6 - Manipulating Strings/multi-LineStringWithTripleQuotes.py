3f341e01f14a24ceb71e711a0337b0f6e567cb23
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-20 14:56:45 -0400

    Triple quotes ''' for multiline strings.

---
+++ Part 1 - Python Programming Basics/Ch. 6 - Manipulating Strings/multi-LineStringWithTripleQuotes.py
@@ -0,0 +1,14 @@
+# While you can use the \n escape character to put a newline into  a string, it is often easier to
+# use multiline strings.  A multiline string in Python begins and ends with either three single quotes
+# or three double quotes. Any quotes, tabs, or newlines in between the "triple quotes" are considered
+# part of the string.
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+# Notice that the single quote in "Eve's" did not need to be escaped.
+
