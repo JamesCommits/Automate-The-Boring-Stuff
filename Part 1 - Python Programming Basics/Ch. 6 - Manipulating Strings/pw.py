fc196918ddea37d070ff0244aa203de1fafc058e
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-22 11:20:10 -0400

    Insecure Password Locker - using sysargv

--- Part 1 - Python Programming Basics/Ch. 6 - Manipulating Strings/pw.py
+++ Part 1 - Python Programming Basics/Ch. 6 - Manipulating Strings/pw.py
@@ -4,10 +4,16 @@
 PASSWORDS = {'email' : 'F3fdf9r1fg39d', 'blog' : '4tgf233rgsg354FS', 'luggage' : '12345'}
 
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
-
+# sys.argv is a LIST[] that contains the arguments a user passes through the command line.
 account = sys.argv[1]  # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for {} has been copied to the clipboard!'.format(account))
+else:
+    print('There is no account named {}'.format(account))
