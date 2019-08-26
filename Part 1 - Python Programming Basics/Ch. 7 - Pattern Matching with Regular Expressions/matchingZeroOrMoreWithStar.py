601d903f99f471073fc39aa78b9a60af2a705905
JamesCommits <51542385+JamesCommits@users.noreply.github.com>
2019-08-26 16:11:29 -0400

    Using * to match zero or more

---
+++ Part 1 - Python Programming Basics/Ch. 7 - Pattern Matching with Regular Expressions/matchingZeroOrMoreWithStar.py
@@ -0,0 +1,18 @@
+# The * symbol means 'match zero or more' - the group that
+# precedes the star can occur any number of times in the text.
+# It can be completely absent or repeated over and over again.
+
+import re
+
+batRegex = re.compile(r'Bat(wo)*man')
+mo1 = batRegex.search('The Adventures of Batman')
+
+print(mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwoman')
+
+print(mo2.group())
+
+mo3 = batRegex.search('The Adventures of Batwowowowowman')
+
+print(mo3.group())
